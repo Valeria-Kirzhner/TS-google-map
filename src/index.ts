77f9b4d5cbf9134ac86eef6bdd8c7a11977e5583
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-08-11 15:58:00 +0300

    added google map aoi result on html element.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-import { User } from "./User";
-import { Company } from "./Company";
+/// <reference types="@types/google.maps" />
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
+// const user = new User();
+// const company = new Company();
 
-console.log(user);
-console.log(company);
+// console.log(user);
+// console.log(company);
 
-google;
+// google;
+const portalDiv = document.getElementById("map")!;
+function initMap() {
+  const map = new google.maps.Map(portalDiv, {
+    zoom: 1,
+    center: {
+      lat: 0,
+      lng: 0,
+    },
+  });
+  return map;
+}
+initMap();
+
+//directionsDisplay.setMap(map);
+//AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU
