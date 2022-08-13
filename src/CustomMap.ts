a23bea5c4a655b6a68924420286cca4f4f4b72ec
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-08-13 09:34:55 +0300

    added listener on click marker popup message.

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -20,12 +20,19 @@ export class CustomMap {
     });
   }
   addMarker(mappable: Mappble): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi There",
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
